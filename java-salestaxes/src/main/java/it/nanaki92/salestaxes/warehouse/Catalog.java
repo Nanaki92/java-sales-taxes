6231023d651e79e4341ec756ac10a30feb61583c
Nanaki92 <andrea.ferrario92@gmail.com>
2019-06-17 19:50:14 +0200

    added catalog cases

--- java-salestaxes/src/main/java/it/nanaki92/salestaxes/warehouse/Catalog.java
+++ java-salestaxes/src/main/java/it/nanaki92/salestaxes/warehouse/Catalog.java
@@ -14,6 +14,8 @@ public class Catalog {
 		catalog.put("music CD", OTHER);
 		catalog.put("box of chocolates", FOOD);
 		catalog.put("bottle of perfume", OTHER);
+		catalog.put("packet of headache pills", MEDICAL);
+		catalog.put("chocolate bar", FOOD);
 	}
 	
 	public Category getCategory(String productName) {
